cce726bf2e0b6972628a81b2e46466b430414d7f
Suvankar <suvankarpradhan100@gmail.com>
2021-04-17 21:39:53 +0530

    mobile version add

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $('.share').click(()=>{
 $('.visibility').toggleClass('popup');
+if($(window).width() < 375)
+{
+    if($('.article-writer').css("background-color")==='rgb(72, 85, 106)'){
+        $('.article-writer').css('background-color','white');
+    }else{
+        $('.article-writer').css('background-color','rgb(72, 85, 106)');
+    }
+}
 })
