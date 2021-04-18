943999d083b56fe064fedde44334254c9123b8cb
Suvankar <suvankarpradhan100@gmail.com>
2021-04-18 12:13:20 +0530

    share button add in mobile version

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ $('.share').click(()=>{
 $('.visibility').toggleClass('popup');
 if($(window).width() < 375)
 {
-    if($('.article-writer').css("background-color")==='rgb(72, 85, 106)'){
+    if($('.article-writer').css('background-color')==='rgb(72, 85, 106)'){
         $('.article-writer').css('background-color','white');
+        $('.share').css('background-color','hsl(210, 46%, 95%)');
+        $('.share>img').attr('src','images/icon-share.svg');
     }else{
         $('.article-writer').css('background-color','rgb(72, 85, 106)');
+        $('.share').css('background-color','hsl(214, 17%, 51%)');
+        $('.share>img').attr('src','images/share.ico');        
     }
 }
 })
